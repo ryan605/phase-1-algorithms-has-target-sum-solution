83a9878251a3c310812b9b004a5d496e863d0536
ryan605 <langatryan26@gmail.com>
2022-12-14 08:30:43 +0300

    completed

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+   for(let i = 0; i < array.length; i++)
+  {
+    for(let j = i + 1; j < array.length; j++)
+    {
+      let sum = array[i] + array[j]
+      console.log(sum, target)
+      if(sum === target)
+      {
+        return true
+      }
+   
+    }
+  }
+  return false
 }
 
 /* 
